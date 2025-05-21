361188e1cd1dc962961c755c8bcd4d92ef732d17
Aya-Mohammed-Hussien <aya.mohamed.hussien@gmail.com>
2025-05-21 17:23:29 +0300

    first commit

---
+++ javascript/main.js
@@ -0,0 +1,11 @@
+//show hide toggle 
+// $(".test").show(1000);
+// $(".test").hide(1000);
+// $(".test").toggle(1000);
+// slideUP slideDown slideToggle 
+// $(".test").slideUp(1000);
+// $(".test").slideDown(1000);
+// $(".test").slideToggle(1000)
+//fadeIn fadeOut fadeToggle 
+// $(".test").fadeIn(10000)
+$(".test").fadeTo(5000 , 0.7); //0.7 is the opacity i want the element to have and in this case display:none is not an indicator 
